e23b7808eaa070404af270663032f1551a86c40e
Varun <varunvaru524@gmail.com>
2023-01-08 13:34:53 +0530

    Added Time Duedate Status and Tag functionaity to ActivityClass Component

--- src/Assets/BackendService.js
+++ src/Assets/BackendService.js
@@ -4,8 +4,8 @@ const activities = [
     title: "Terminator",
     timeStamp: '7/1/2023 2:50:49 am',
     description: 2.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-21',
+    tags:'urgent',
     status:'open'
   },
   {
@@ -13,8 +13,8 @@ const activities = [
     title: "Die Hard",
     timeStamp: '23/12/2022 3:50:49 pm',
     description: 2.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-20',
+    tags:'highPriority',
     status:'working'
   },
   {
@@ -22,8 +22,8 @@ const activities = [
     title: "Get Out",
     timeStamp: '3/6/2021 1:50:49 pm',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-25',
+    tags:'important',
     status:'done'
   },
   {
@@ -31,8 +31,8 @@ const activities = [
     title: "Trip to Italy",
     timeStamp: '17/10/2022 12:50:49 am',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-25',
+    tags:'urgent',
     status:'overDue'
   },
   {
@@ -40,8 +40,8 @@ const activities = [
     title: "Airplane",
     timeStamp: '15/6/2022 10:03:49 am',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-25',
+    tags:'notImportant',
     status:'open'
   },
   {
@@ -49,8 +49,8 @@ const activities = [
     title: "Wedding Crashers",
     timeStamp: '02/3/2022 2:53:49 am',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-01-25',
+    tags:'meeting',
     status:'working'
   },
   {
@@ -58,8 +58,8 @@ const activities = [
     title: "Gone Girl",
     timeStamp: '30/8/2021 07:53:49 am',
     description: 4.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-02-03',
+    tags:'meeting',
     status:'done'
   },
   {
@@ -67,8 +67,8 @@ const activities = [
     title: "The Sixth Sense",
     timeStamp: '05/5/2022 5:34:49 pm',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-02-03',
+    tags:'urgent',
     status:'overDue'
   },
   {
@@ -76,8 +76,8 @@ const activities = [
     title: "The Avengers",
     timeStamp: '4/2/2022 6:53:49 pm',
     description: 3.5,
-    dueDate:'date',
-    tags:[],
+    dueDate:'2023-02-01',
+    tags:'notImportant',
     status:'open'
   }
 ];
@@ -90,20 +90,22 @@ export function getActivity(id) {
   return activities.find(a => a._id === id);
 }
 
-// export function saveMovie(movie) {
-//   let movieInDb = movies.find(m => m._id === movie._id) || {};
-//   movieInDb.name = movie.name;
-//   movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-//   movieInDb.numberInStock = movie.numberInStock;
-//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
+export function saveActivity(activity) {
+  let activityInDb = activity.find(m => m._id === activity._id) || {};
+  activityInDb.title = activity.title;
+  activityInDb.timeStamp = activity.time;
+  activityInDb.description = activity.description;
+  activityInDb.dueDate = activity.dueDate;
+  activityInDb.tags = activity.tag;
+  activityInDb.status = activity.status
 
-//   if (!movieInDb._id) {
-//     movieInDb._id = Date.now();
-//     movies.push(movieInDb);
-//   }
+  if (!activityInDb._id) {
+    activityInDb._id = Date.now();
+    activities.push(activityInDb);
+  }
 
-//   return movieInDb;
-// }
+  return activityInDb;
+}
 
 export function deleteActivity(id) {
   let activityInDb = activities.find(a => a._id === id);
--- src/Components/Activity/ActivityClass.jsx
+++ src/Components/Activity/ActivityClass.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { getActivity } from '../../Assets/BackendService';
 
 class ActivityClass extends Component {
     state = {
-        userInput:{time:'',title:'',description:'',dueDate:'',tag:'',status:''},
+        userInput:{time:'',title:'',description:'',dueDate:'',tag:'',status:'',_id:''},
         errors:{}
+    }
+
+    componentDidMount(){
+        if (this.props.params.id == "new") {
+            let newUserInput = this.state.userInput
+
+            let date = new Date()
+            newUserInput.time = date.toLocaleDateString()+' '+date.toLocaleTimeString()
+            
+            this.setState({userInput:newUserInput})
+        }
+        else {
+            let data = getActivity(this.props.params.id)
+            let updatedUserInput = {
+                time: data.timeStamp,
+                title: data.title,
+                description: data.description,
+                dueDate: data.dueDate,
+                tag: data.tags,
+                status: data.status,
+                _id: data._id
+            }
+            this.setState({userInput:updatedUserInput})
+        }
     }
 
     handleChange({currentTarget}){
@@ -15,6 +40,25 @@ class ActivityClass extends Component {
         
         this.setState({userInput:updatedUserInput})
     }
+    
+    disablePreviousDate(){
+        let today = new Date();
+        let month = today.getMonth() + 1;
+        let day = today.getDate();
+        let year = today.getFullYear();
+        if(month < 10) month = '0' + month.toString();
+        if(day < 10) day = '0' + day.toString();
+    
+        return year + '-' + month + '-' + day;
+    }
+
+    handleDetele(){
+
+    }
+
+    handleValidation(){
+
+    }
 
     handleSubmit(event){
         event.preventDefault()
@@ -23,14 +67,13 @@ class ActivityClass extends Component {
     render() { 
         let {params} = this.props
         let {description,dueDate,status,tag,time,title} = this.state.userInput
-        console.log(title);
         return (
             <React.Fragment>
                 <h1>Activity</h1>
                 <form onSubmit={(e)=>this.handleSubmit(e)}>
                     <div className="timeStampContainer">
                         <p className='timeStamp'>The Time</p>
-                        <p className='time'>Time</p>
+                        <p className='time'>{time}</p>
                     </div>
                     <div className='titleContainer'>
                         <p className='title'>Title</p>
@@ -39,22 +82,28 @@ class ActivityClass extends Component {
                     </div>
                     <div className='descriptionContainer'>
                         <p className='description'>Description</p>
-                        <input type="text" onChange={(e)=>this.handleChange(e)} name='description' placeholder='Enter Description' />
+                        <input type="text" onChange={(e)=>this.handleChange(e)} value={description} name='description' placeholder='Enter Description' />
                         <p className='errors'>errors</p>
                     </div>
                     <div className='dueDateContainer'>
                         <p className='dueDate'>Due Date</p>
-                        <input type="text" />
+                        <input min={this.disablePreviousDate()}  type="date" onChange={(e)=>this.handleChange(e)} value={dueDate} name="dueDate" />
                         <p className='errors'>errors</p>
                     </div>
                     <div className='TagContainer'>
                         <p className='tag'>Tag</p>
-                        <input type="text" placeholder='Enter Tag Name' />
+                        <select name='tag' onChange={(e)=>this.handleChange(e)} value={tag}>
+                            <option value="important">Important</option>
+                            <option value="urgent">Urgent</option>
+                            <option value="notImportant">Not Important</option>
+                            <option value="highPriority">High Priority</option>
+                            <option value="meeting">Meeting</option>
+                        </select>
                         <p className='errors'>errors</p>
                     </div>
                     <div className='statusContainer'>
                         <p className='status'>Status</p>
-                        <select>
+                        <select name='status' onChange={(e)=>this.handleChange(e)} value={status}>
                             <option value="open">Open</option>
                             <option value="working">Working</option>
                             <option value="done">Done</option>
